cd52d24d9cd1fd2882c7b0498909879fb09b4606
Ramanuz4 <ramanuzkashyap4@gmail.com>
2024-08-21 19:05:10 +0530

    add button and the constitution div

---
+++ script.js
@@ -0,0 +1,13 @@
+const indianConstitution = [
+  {
+    article: "Preamble",
+    content:
+      "WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a 1. [SOVEREIGN SOCIALIST SECULAR DEMOCRATIC REPUBLIC] and to secure to all its citizens:JUSTICE, social, economic and political;LIBERTY of thought, expression, belief, faith and worship;EQUALITY of status and of opportunity;and to promote among them all FRATERNITY assuring the dignity of the individual and the 2. [unity and integrity of the Nation]; IN OUR CONSTITUENT ASSEMBLY this twenty-sixth day of November, 1949, do HEREBY ADOPT, ENACT AND GIVE TO OURSELVES THIS CONSTITUTION.",
+  },
+  {
+    article: "Article 1",
+    content:
+      "Name and territory of the Union.—(1) India, that is Bharat, shall be a Union of States.(2) The States and the territories thereof shall be as specified in the First Schedule.",
+  },
+  // Add more articles here
+];
